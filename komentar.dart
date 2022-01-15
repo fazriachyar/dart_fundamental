95a5c1138cb53fbdfb0e498316bb0d49452dee76
fazriachyar <69377603+fazriachyar@users.noreply.github.com>
2022-01-15 09:54:01 +0700

    komentar dart
    penjelasan penggunaan komentar dalam dokumentasi kode

---
+++ komentar.dart
@@ -0,0 +1,13 @@
+import 'package:test1/main.dart';
+
+void main(){
+  //Komentar 1 baris
+  /// Komentar mengarah pada suatu class [HomeScreen]
+  /*
+    Komentar lebih dari 1 baris
+   */
+
+  /**
+   * komentar lebih dari satu baris mengarah pada suatu class [HomeScreen]
+   */
+}
