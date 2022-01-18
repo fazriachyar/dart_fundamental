7ccbaa8cf194de8931eead44291fec568183600e
fazriachyar <69377603+fazriachyar@users.noreply.github.com>
2022-01-18 16:08:13 +0700

    Create nullsafety.dart

---
+++ nullsafety.dart
@@ -0,0 +1,17 @@
+void main(){
+  //NULL SAFETY
+  //ada beberapa hal penting dari null safety
+  /*
+    1. ? = berguna untuk memperbolehkan suatu variabel bernilai null
+    2. ?? = menangani null safety
+    3. ! = memastikan bahwa variabel tidak null dan memiliki value
+  
+   */
+
+  int? nilai = null;
+
+
+
+
+
+}
